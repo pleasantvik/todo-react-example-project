69f6c3f66abc6aaf97efe1398823d4f187cd1b17
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-12-20 11:25:06 +0100

    project start

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+const App = () => {
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      title: "Start teaching React",
+      completed: true,
+    },
+
+    {
+      id: 2,
+      title: "Build the UI",
+      completed: false,
+    },
+
+    {
+      id: 3,
+      title: "Deploy app on vercel",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "Read on JSX",
+      completed: true,
+    },
+  ];
+  // The first thine we want to do is display the title in the UI.
+  // To accomplish this we use the map method available on every JS object
+
+  return <div className="App"></div>;
+};
+
+export default App;
