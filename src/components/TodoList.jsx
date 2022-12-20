de795b62a1703da9b44524ff0e766432ea4b5fa8
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-12-20 12:15:41 +0100

    todoList showing the list of todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,14 @@
 export const TodoList = (props) => {
   console.log(props.todos);
   return (
-    <ul>
-      {props.todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
+    <>
+      <h2>TodoList Component</h2>
+      <p>I am shpwing the list of todo now</p>
+      <ul>
+        {props.todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
   );
 };
